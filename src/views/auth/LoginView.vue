8051021745fb70f95f1e38d12e227eacfc442244
GHINonan <giannonan@gmail.com>
2024-12-12 19:17:30 +0800

    added applayout

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,66 +1,39 @@
 <script setup>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-14">
+          <h1 class="text-center">SkillHunt</h1>
+          <p class="text-center">Your No.1 Choice for Public Outsourcing!</p>
+          <br />
+          <v-card class="mx-auto, text-center" prepend-icon="mdi-account" variant="outlined">
+            <template v-slot:title>
+              <span class="font-weight-black">Please Log In to Continue</span>
+            </template>
 
-        <v-btn
-          variant="tonal"
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="Email Address" variant="outlined"></v-text-field>
 
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto">
-              <h1 class="text-center">SkillHunt</h1>
-              <p class="text-center">Your No.1 Choice for Public Outsourcing!</p>
-              <br />
-              <v-card class="mx-auto, text-center" prepend-icon="mdi-account">
-                <template v-slot:title>
-                  <span class="font-weight-black">Please Log In to Continue</span>
-                </template>
-
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <v-text-field label="Email Address" variant="outlined"></v-text-field>
-
-                    <v-text-field
-                      label="Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-                    <v-btn class="mt-2" type="submit" block>Log In</v-btn>
-                    <v-divider class="my-4"></v-divider>
-                    <h5>
-                      No account?
-                      <RouterLink to="/register">Click here to Register instead!</RouterLink>
-                    </h5>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-      <v-footer border app
-        >"When you have the chance to make a choice, make one that you know you won't
-        regret."</v-footer
-      >
-    </v-app>
-  </v-responsive>
+                <v-text-field label="Password" type="password" variant="outlined"></v-text-field>
+                <v-btn class="mt-2" type="submit" block variant="outlined" rounded="lg"
+                  >Log In</v-btn
+                >
+                <v-divider class="my-4"></v-divider>
+                <h5>
+                  No account?
+                  <RouterLink to="/register">Click here to Register instead!</RouterLink>
+                </h5>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
+  <
 </template>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,71 +1,48 @@
-<script>
-import { ref } from 'vue'
-
-const theme = ref('light')
-
-function onClick() {
-  theme.value = theme.value === 'light' ? 'dark' : 'light'
-}
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
 </script>
 
 <template>
-  <v-responsive class="border rounded">
-    <v-app :theme="theme">
-      <v-app-bar class="px-3">
-        <v-spacer></v-spacer>
+  <AppLayout>
+    <template #content>
+      <v-row>
+        <v-col cols="12" md="6" class="mx-auto pt-10">
+          <h1 class="text-center">SkillHunt</h1>
+          <p class="text-center">Your No.1 Choice for Public Outsourcing!</p>
+          <br />
+          <v-card class="mx-auto, text-center" prepend-icon="mdi-account" variant="outlined">
+            <template v-slot:title>
+              <span class="font-weight-black">Create an Account</span>
+            </template>
 
-        <v-btn
-          :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
-          text="Toggle Theme"
-          slim
-          @click="onClick"
-        ></v-btn>
-      </v-app-bar>
-
-      <v-main>
-        <v-container>
-          <v-row>
-            <v-col cols="12" md="6" class="mx-auto">
-              <h1 class="text-center">SkillHunt</h1>
-              <p class="text-center">Your No.1 Choice for Public Outsourcing!</p>
-              <br />
-              <v-card class="mx-auto, text-center" prepend-icon="mdi-account">
-                <template v-slot:title>
-                  <span class="font-weight-black">Create an Account</span>
-                </template>
-
-                <v-card-text class="bg-surface-light pt-4">
-                  <v-form fast-fail @submit.prevent>
-                    <!--<v-row>
-                      <v-col></v-col>
-                      <v-col></v-col> </v-row
-                    >/-->
-                    <v-text-field label="First Name" variant="outlined"></v-text-field>
-                    <v-text-field label="Last Name" variant="outlined"></v-text-field>
-                    <v-text-field label="Email Address" variant="outlined"></v-text-field>
-                    <v-text-field
-                      label="Enter Password"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-                    <v-text-field
-                      label="Enter Password Again to Confirm"
-                      type="password"
-                      variant="outlined"
-                    ></v-text-field>
-                    <v-btn class="mt-2" type="submit" block>Register</v-btn>
-                    <v-divider class="my-4"></v-divider>
-                    <h5>
-                      Already have an account?
-                      <RouterLink to="/login">Click Here To Log In!</RouterLink>
-                    </h5>
-                  </v-form>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+            <v-card-text class="bg-surface-light pt-4">
+              <v-form fast-fail @submit.prevent>
+                <v-text-field label="First Name" variant="outlined"></v-text-field>
+                <v-text-field label="Last Name" variant="outlined"></v-text-field>
+                <v-text-field label="Email Address" variant="outlined"></v-text-field>
+                <v-text-field
+                  label="Enter Password"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
+                <v-text-field
+                  label="Enter Password Again to Confirm"
+                  type="password"
+                  variant="outlined"
+                ></v-text-field>
+                <v-btn class="mt-2" type="submit" block variant="outlined" rounded="lg"
+                  >Register</v-btn
+                >
+                <v-divider class="my-4"></v-divider>
+                <h5>
+                  Already have an account?
+                  <RouterLink to="/login">Click Here To Log In!</RouterLink>
+                </h5>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </AppLayout>
 </template>
