810ff11d92e0c3e92c52811d5a307585ccdce612
GHINonan <giannonan@gmail.com>
2024-12-13 11:30:45 +0800

    adjusted border and footer

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -9,7 +9,7 @@ function onClick() {
 </script>
 
 <template>
-  <v-responsive class="border rounded">
+  <v-responsive>
     <v-app :theme="theme">
       <v-app-bar class="px-3">
         <v-spacer></v-spacer>
@@ -27,7 +27,7 @@ function onClick() {
           <slot name="content"></slot>
         </v-container>
       </v-main>
-      <v-footer border app
+      <v-footer class="font-weight-bold" border app
         >"When you have the chance to make a choice, make one that you know you won't
         regret."</v-footer
       >
