6bf3fe23db16bf75d3830a7b1b75b747ed6a516f
GHINonan <giannonan@gmail.com>
2024-12-13 13:51:19 +0800

    fix: changed various components in views

--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -6,25 +6,46 @@ const visible = ref(false)
 
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field label="First Name" variant="outlined"></v-text-field>
-    <v-text-field label="Last Name" variant="outlined"></v-text-field>
-    <v-text-field label="Email Address" variant="outlined" prepend-icon="mdi-email"></v-text-field>
-    <v-text-field
-      label="Enter Password"
-      variant="outlined"
-      prepend-icon="mdi-lock"
-      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-      :type="visible ? 'text' : 'password'"
-      @click:append-inner="visible = !visible"
-    ></v-text-field>
-    <v-text-field
-      label="Enter Password Again to Confirm"
-      variant="outlined"
-      prepend-icon="mdi-lock-check"
-      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-      :type="visible ? 'text' : 'password'"
-      @click:append-inner="visible = !visible"
-    ></v-text-field>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field
+          prepend-icon="mdi-account"
+          label="First Name"
+          variant="outlined"
+        ></v-text-field
+      ></v-col>
+      <v-col cols="12" md="6"
+        ><v-text-field label="Last Name" variant="outlined"></v-text-field
+      ></v-col>
+      <v-col cols="12"
+        ><v-text-field
+          label="Email Address"
+          variant="outlined"
+          prepend-icon="mdi-email"
+        ></v-text-field
+      ></v-col>
+      <v-col cols="12"
+        ><v-text-field
+          label="Enter Password"
+          variant="outlined"
+          prepend-icon="mdi-lock"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          @click:append-inner="visible = !visible"
+        ></v-text-field
+      ></v-col>
+      <v-col cols="12"
+        ><v-text-field
+          label="Enter Password Again to Confirm"
+          variant="outlined"
+          prepend-icon="mdi-lock-check"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          @click:append-inner="visible = !visible"
+        ></v-text-field
+      ></v-col>
+    </v-row>
+
     <v-btn
       class="mt-2"
       type="submit"
